9e1ff2792be7c054b7cd4a89bc10999d2770063b
jjiunnbeh <jjiunnbeh@gmail.com>
2024-03-09 01:46:15 +0800

    Add notepad component and initial styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import '../App.css';
 import Footer from "./Footer";
 import Header from "./Header";
+import NotePad from "./Notepad";
 
 function App()
 {
   return(
   <>
   <h1><Header /></h1>
+  <NotePad />
   <h1><Footer /></h1>
   </>
   );
--- src/components/Notepad.tsx
+++ src/components/Notepad.tsx
@@ -1,4 +1,11 @@
 function NotePad()
 {
-    
+    return (
+        <div className="notepad">
+        <h1>Title</h1>
+        <p>Content</p>
+        </div>
+    );
 }
+
+export default NotePad;
