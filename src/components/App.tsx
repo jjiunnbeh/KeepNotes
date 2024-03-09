20cca144b65e4b7fa0046af8fecda8650d547883
jjiunnbeh <jjiunnbeh@gmail.com>
2024-03-10 05:06:12 +0800

    Add Notes.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,18 +7,12 @@ import Notes from "./Notes";
 
 function App()
 {
-  
-  
-
-
-  let num = 5
   return(
   <>
   <h1><Header /></h1>
   {Notes.length != 0 && Notes.map(Note => <NotePad key={Note.id} title={Note.title} content={Note.content}/>)}
   <h1><Footer /></h1>
   </>
-  );
-  
+  ); 
 }
 export default App;
